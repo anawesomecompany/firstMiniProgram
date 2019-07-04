1e5b414ae226b64d6445ed1c9d2bff21f9d003bd
豆有喆 <oncwnuHmu8kQbFtx76VvmPM9pO5c@git.weixin.qq.com>
2019-07-04 15:42:48 -0700

    added home and welcome page

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,15 +1,15 @@
 // pages/home/home.js
 Page({
-  navigateToAdd:function(event){
-    wx.navigateTo({
-      url: '../add/add',
-    })
-  },
+  // navigateToAdd:function(event){
+  //   wx.navigateTo({
+  //     url: '../add/add',
+  //   })
+  // },
 
-  navigateToGet: function (event) {
-    wx.navigateTo({
-      url: '../get/get',
-    })
-  }
+  // navigateToGet: function (event) {
+  //   wx.navigateTo({
+  //     url: '../get/get',
+  //   })
+  // }
 
 })
---
+++ miniprogram/pages/welcome/welcome.js
@@ -0,0 +1,10 @@
+// pages/welcome/welcome.js
+Page({
+
+  navigateToHome: function (event) {
+    wx.navigateTo({
+      url: '../home/home',
+    })
+  }
+
+})
